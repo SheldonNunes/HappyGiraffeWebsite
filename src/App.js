0f2c67be7ed12eec3f3942aa39834ec5f7432d3b
Sheldon Nunes <sheldon.nunes02@gmail.com>
2018-09-12 20:25:23 +1200

    Change updates the vertical height and flex contents so that the website is centered vertically

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        <h1 className='about__title'>Happy Giraffe</h1>
         <div>
-            <img src={logo} className='logo-center' alt="Happy Giraffe Logo" />
-        </div>
-        <div className='about__description'>
-            <h2>Welcome to Happy Giraffe</h2>
-            <p>I am a software developer with keen interest on all things tech</p>
+          <h1 className='about__title'>Happy Giraffe</h1>
+          <div>
+              <img src={logo} className='logo-center' alt="Happy Giraffe Logo" />
+          </div>
+          <div className='about__description'>
+              <h2>Welcome to Happy Giraffe</h2>
+              <p>I am a software developer with keen interest on all things tech</p>
+          </div>
         </div>
       </div>
     );
