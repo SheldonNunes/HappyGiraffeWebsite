c2e3ca1096025657cb3d3f0a29750c5b3a861777
Sheldon Nunes <sheldon.nunes02@gmail.com>
2018-11-15 22:05:20 +1300

    Update image width (#2)
    * Change updates image width of blog posts
    
    * Change adds loading spinner

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -2,22 +2,29 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import Post from './../componenets/Post'
 import ReactGA from 'react-ga';
+import Spinner from './../componenets/Spinner';
 
 export default class Blog extends React.Component {
     state = {
-      posts: []
+      posts: [],
+      loading: false
     }
 
     componentDidMount() {
+      var that = this;
+      this.setState({ loading: true })
       axios.get(`https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2Fhappy-giraffe`)
       .then(response => {
           return response.data.items
       })
       .then(posts => {
-          this.setState({ posts })
+        that.setState({ posts })
       })
       .catch(function (error) {
         console.log(error);
+      })
+      .finally(function() {
+        that.setState({ loading: false })
       });
     }
 
@@ -27,6 +34,7 @@ export default class Blog extends React.Component {
         <div className="blog">
           <h2>Blog</h2>
             <div className="posts">
+            { this.state.loading ? <Spinner /> : null }
               { this.state.posts.map((post) => 
                 <Post key={post.title} content={post} />)
               }
