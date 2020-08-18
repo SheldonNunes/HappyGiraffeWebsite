6b5709c0d96fc82954b02709525f3e6846cb0c5b
Sheldon Nunes <sheldon.nunes02@gmail.com>
2020-08-18 08:53:34 -0400

    change adds new position

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,8 @@
-import React, { Component } from "react";
+import React, { Component, useRef } from "react";
 import logo from "./../images/giraffe-logo.png";
 import banner from "./../images/banner.jpg";
 import ReactGA from "react-ga";
+import here from "./../images/we-are-here.png";
 import ReactMapboxGl, {
   Layer,
   Feature,
@@ -17,6 +18,14 @@ const Map = ReactMapboxGl({
 });
 
 export default function Home(props) {
+  const ref = React.createRef();
+
+  const handleClick = () =>
+    ref.current.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+
   ReactGA.pageview("/t");
   return (
     <div className="home">
@@ -28,27 +37,24 @@ export default function Home(props) {
         <img src={banner} className="banner" />
         <div className="banner-footer">
           <p>See where we are now</p>
-          <p className="arrow">▽</p>
+          <p onClick={handleClick} className="arrow">
+            ▽
+          </p>
         </div>
       </div>
-      <div>
+      <div ref={ref}>
         <Map
           style="mapbox://styles/mapbox/streets-v9"
           containerStyle={{
-            height: "700px",
+            height: "100vh",
             width: "100%",
             cursor: "default",
           }}
-          center={[-69.000119, 47.341503]}
+          center={[-66.1007, 48.0982]}
           zoom={[6]}
         >
-          <Marker coordinates={[-66.542361, 48.07969]} anchor="bottom">
-            <img
-              className="we-are-here"
-              src={
-                "https://static.wixstatic.com/media/9447b0_1f2aa0b7d11c47468126d5163821ee91~mv2_d_2708_4097_s_4_2.png/v1/fill/w_600,h_908,al_c,q_90,usm_0.66_1.00_0.01/9447b0_1f2aa0b7d11c47468126d5163821ee91~mv2_d_2708_4097_s_4_2.webp"
-              }
-            />
+          <Marker coordinates={[-66.1007, 48.0982]} anchor="bottom">
+            <img className="we-are-here" src={here} />
           </Marker>
           <GeoJSONLayer
             data={features}
