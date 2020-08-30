46518fafa96f185b5fa99418693a0e58c0b63f56
Sheldon Nunes <sheldon.nunes02@gmail.com>
2020-08-30 19:27:18 -0400

    update after magdolen islands

--- src/componenets/TimelineElement.js
+++ src/componenets/TimelineElement.js
@@ -10,11 +10,11 @@ export default function TimelineElement(props) {
   return (
     <VerticalTimelineElement
       className="vertical-timeline-element--work"
-      contentStyle={{ background: "rgb(33, 150, 243)", color: "#222" }}
-      contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
-      date={timeline_location.date}
-      iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-      //   icon={<img src={logo}></img>}
+      contentStyle={{ background: "rgb(137 154 71)", color: "#222" }}
+      contentArrowStyle={{ borderRight: "7px solid  rgb(137 154 71)" }}
+      // date={timeline_location.date}
+      iconStyle={{ background: "rgb(137 154 71)", color: "#fff" }}
+      icon={<span className="timeline-date">{timeline_location.date}</span>}
     >
       <img className="timeline-image" src={timeline_location.image}></img>
       <h3 className="vertical-timeline-element-title">
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,7 @@
 import React, { Component, useRef, useState, useEffect } from "react";
 import logo from "./../images/giraffe-logo.png";
+import film_roll from "./../images/film-roll.png";
+import sony_a7 from "./../images/sony-a7ii.png";
 import banner from "./../images/banner.jpg";
 import ReactGA from "react-ga";
 import here from "./../images/van-gif2.gif";
@@ -8,6 +10,7 @@ import Timeline from "./../componenets/Timeline";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 import "./../Odometer.css";
+import "./../ProgressBar.css";
 import ReactMapboxGl, {
   Layer,
   Feature,
@@ -26,7 +29,7 @@ const Map = ReactMapboxGl({
   accessToken:
     "pk.eyJ1Ijoic2hlbGRvbm51bmVzIiwiYSI6ImNrZHhqd3k1bDEyZXkycm9nOWdjM3hvNHAifQ.U1ml7ludEhsFESNWh062Qg",
 });
-const distanceTravelled = 241354 - 238904;
+const distanceTravelled = 242897 - 238904;
 const currentDays = Math.round(
   (Date.now() - Date.parse("01 Aug 2020 00:00:00 EST")) / (1000 * 60 * 60 * 24)
 );
@@ -75,60 +78,65 @@ export default function Home(props) {
           </p>
         </div>
       </div>
-      <div className="map-container" ref={ref}>
-        <Map
-          style="mapbox://styles/sheldonnunes/cke4f32ub037k18pdnv70maw9"
-          containerStyle={{
-            height: "100vh",
-            width: "100%",
-            cursor: "default",
-          }}
-          center={[-61.771633, 47.413498]}
-          zoom={[6]}
-        >
-          <Marker coordinates={[-61.771633, 47.413498]} anchor="center">
-            <img className="we-are-here" src={here} />
-          </Marker>
-          <GeoJSONLayer
-            data={travelled}
-            lineLayout={{}}
-            linePaint={{
-              "line-width": 2,
+      <div className="map-section" ref={ref}>
+        <div className="map-container">
+          <Map
+            style="mapbox://styles/sheldonnunes/cke4f32ub037k18pdnv70maw9"
+            containerStyle={{
+              height: "100%",
+              width: "100%",
+              cursor: "default",
             }}
-            symbolLayout={{
-              "text-field": "{place}",
-              "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
-              "text-offset": [0, 0.6],
-              "text-anchor": "top",
-            }}
-          />
-          <GeoJSONLayer
-            data={locations}
-            lineLayout={{}}
-            circlePaint={{
-              "circle-radius": 6,
-            }}
-            linePaint={{
-              "line-width": 2,
-            }}
-            symbolLayout={{
-              "text-field": "{place}",
-              "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
-              "text-offset": [0, 0.6],
-              "text-anchor": "top",
-            }}
-            circleOnClick={(circleInfo) => {
-              setSelectedLocation(circleInfo.features[0]);
-            }}
-          />
-          {/* <ZoomControl position="top-left" /> */}
-        </Map>
-        <div className="map-sidebar">
-          <h2>Some Statistics</h2>
+            center={[-64.677986, 47.47371]}
+            zoom={[6]}
+          >
+            <Marker coordinates={[-64.677986, 48.347371]} anchor="center">
+              <img className="we-are-here" src={here} />
+            </Marker>
+            <GeoJSONLayer
+              data={travelled}
+              lineLayout={{}}
+              linePaint={{
+                "line-width": 2,
+              }}
+              symbolLayout={{
+                "text-field": "{place}",
+                "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+                "text-offset": [0, 0.6],
+                "text-anchor": "top",
+              }}
+            />
+            <GeoJSONLayer
+              data={locations}
+              lineLayout={{}}
+              circlePaint={{
+                "circle-radius": 6,
+              }}
+              linePaint={{
+                "line-width": 2,
+              }}
+              symbolLayout={{
+                "text-field": "{place}",
+                "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+                "text-offset": [0, 0.6],
+                "text-anchor": "top",
+              }}
+              circleOnClick={(circleInfo) => {
+                setSelectedLocation(circleInfo.features[0]);
+              }}
+            />
+          </Map>
+        </div>
+      </div>
+      <h2>Statistics</h2>
+      <div className="map-sidebar">
+        <div className="full-width">
           <h4>Odometer (kms)</h4>
           <div className="odometer--container">
             <Odometer theme="car" value={distanceTravelled} />
           </div>
+        </div>
+        <div className="full-width">
           <h4>Total Nights</h4>
           <div className="statistic-graph">
             <CircularProgressbar
@@ -136,8 +144,27 @@ export default function Home(props) {
               text={`${currentDays}/${60}`}
             />
           </div>
+        </div>
+        <div className="full-width">
           <h4>Total Tim Horton's visited</h4>
           <p>18</p>
+        </div>
+      </div>
+      <div className="map-sidebar">
+        <div className="full-width">
+          <img className="we-are-here" src={film_roll} />
+          <p>Alpha One - FX-Quartz</p>
+          <p>34/36 Photos</p>
+        </div>
+        <div className="full-width">
+          <img className="we-are-here" src={film_roll} />
+          <p>Alpha Two - FX-Quartz</p>
+          <p>23/36 Photos</p>
+        </div>
+        <div className="full-width">
+          <img className="we-are-here" src={sony_a7} />
+          <p>Sony A7</p>
+          <p>327 Photos</p>
         </div>
       </div>
       <h2>Timeline</h2>
